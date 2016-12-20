7fcb46617026e7a040ce9b832540077e4ca64a1d
phoenix.lv <phoenix.lv@busyberry.com>
2016-12-20 10:35:06 +0800

    reform the README.md and add run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,4 @@
-# ss-server
-ss-server is a Docker image of shadowsocks server with version 2.4.6, built on x86 64-bits.
-
-### Usage
- 
-```
 docker run -t -d --cap-add=NET_ADMIN --network=host \
         --restart=always --name=ss-server --privileged \
         -e LOCAL_PORT=8388 -e PASSWD=test -e ENCRYPT_METHOD=aes-256-cfb \
-        ssserver
-```
+        ssserver 
